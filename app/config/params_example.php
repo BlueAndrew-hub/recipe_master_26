88e1738767d2e8f45af14c98b2aecf8bd0fd4faf
BlueAndrew-hub <ludovicwasson1@gmail.com>
2026-04-09 15:03:15 +0200

    Structure MVC Initiale

---
+++ app/config/params_example.php
@@ -0,0 +1,8 @@
+<?php
+
+$content = "Aucun contenu ne correspond à votre requête.";
+
+define("DB_HOST", "your_host");
+define("DB_NAME", "your_database");
+define("DB_USER", "your_username");
+define("DB_PASSWORD", "your_password");
