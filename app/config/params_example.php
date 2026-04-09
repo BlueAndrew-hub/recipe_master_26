d868df54e3ee7ceebf6a5064ae7b96d988f67667
BlueAndrew-hub <ludovicwasson1@gmail.com>
2026-04-09 15:03:15 +0200

    Structure MVC Initiale

---
+++ app/config/params_example.php
@@ -0,0 +1,10 @@
+<?php
+
+// Initialisation de la zone dynamique $title et $content
+$title = "";
+$content = "Aucun contenu ne correspond à votre requête.";
+
+define("DB_HOST", "your_host");
+define("DB_NAME", "your_database");
+define("DB_USER", "your_username");
+define("DB_PASSWORD", "your_password");
