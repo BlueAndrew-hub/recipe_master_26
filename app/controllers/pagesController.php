36a10c923803545242e3cfc6e290a429b244066a
BlueAndrew-hub <ludovicwasson1@gmail.com>
2026-04-16 15:16:03 +0200

    Route pages hom

---
+++ app/controllers/pagesController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controllers\PagesController;
+
+use \PDO;
+use \App\Models\RecipesModel;
+
+function homeAction(PDO $conn) {
+    include_once '../app/models/recipesModel.php';
+    $randomRecipe = RecipesModel\findOneByRand($conn); 
+
+    GLOBAL $title, $content;
+    $title = "Home";
+    ob_start();
+    include '../app/views/pages/home.php';
+    $content = ob_get_clean();
+}
